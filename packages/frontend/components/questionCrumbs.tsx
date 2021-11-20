ef5c6935ebe2c1c7be8e60af452b50c362e9c6b8
Konrad Gnat <konradmgnat@gmail.com>
2021-11-20 17:26:59 +0100

    question crumbs

--- packages/frontend/components/questionCrumbs.tsx
+++ packages/frontend/components/questionCrumbs.tsx
@@ -8,6 +8,9 @@ type Props = {
 const QuestionCrumbs = (props: Props) => {
     const { questionLabels } = props;
 
+    const handleLabelClick = (index: number) => {
+        console.log('questionLabels', questionLabels[index]);
+    }
     return (
         <Box mb={4}>
             <Flex justify="space-between">
@@ -15,7 +18,18 @@ const QuestionCrumbs = (props: Props) => {
                     questionLabels.map(
                         (label, i) => {
                             return (
-                                <Box key={`questionLabel_${i}`} borderRadius="50%" mr={4} py={2} px={4} border="1px" borderColor={label.isActive ? "pink.600" : label.isCompleted ? "pink.600" : "gray.400"} backgroundColor={label.isCompleted ? 'pink.600': 'gray.800'} >
+                                <Box
+                                    key={`questionLabel_${i}`}
+                                    borderRadius="50%"
+                                    mr={4}
+                                    py={2}
+                                    px={4}
+                                    border="1px"
+                                    borderColor={label.isActive ? "pink.600" : label.isCompleted ? "pink.600" : "gray.400"}
+                                    backgroundColor={label.isCompleted ? 'pink.600': 'gray.800'}
+                                    cursor="pointer"
+                                    onClick={() => handleLabelClick(i)}
+                                >
                                     {label.label}
                                 </Box>
                             )
